761142ca2008caa980750cec97926ecf10d3e9be
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2022-12-15 01:58:08 +0600

    DevOps

--- MOrders/Program.cs
+++ MOrders/Program.cs
@@ -11,6 +11,9 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+*/
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
